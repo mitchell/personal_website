86cb8ab41c4ae19b080033cdf46dfb4ab1f39486
mitchelljfs <mitchelljfsimon@gmail.com>
2017-12-11 20:50:22 -0800

    Recreated my current static website in this sinatra app with the slim template engine

--- website.rb
+++ website.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'yaml'
 
 get '/' do
+  @title = 'Home'
   slim :index, layout: :main
+end
+
+get '/projects' do
+  @title = 'Projects'
+  @projects = YAML.load_file 'data/projects.yml'
+  slim :projects, layout: :main
+end
+
+get '/experience' do
+  @title = 'Experience'
+  slim :experience, layout: :main
+end
+
+get '/contact' do
+  @title = 'Contact'
+  slim :contact, layout: :main
 end
